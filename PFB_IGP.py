9e28ff09f6143c96eab37adf22f7cff57c6114fa
jonathan-galileo <139736148+jonathan-galileo@users.noreply.github.com>
2023-07-23 13:46:26 +0800

    Update PFB_IGP.py

--- PFB_IGP.py
+++ PFB_IGP.py
@@ -37,39 +37,3 @@ print(f"[HIGHEST OVERHEAD] {name_of_overhead}: {percentage_of_overhead}%") # Pri
 
 
 
-MAIN
-# from pathlib import Path
-# from overheads import find_highest_overhead
-# from cash_on_hand import cash_on_hand
-# from profit_loss import profit_loss
-# main= [find_highest_overhead(), cash_on_hand(),profit_loss()]
-
-# import cash_on_hand
-# import overheads
-# import profit_loss
-# result = cash_on_hand
-# print(result)
-
-# fp = Path.cwd()/"summary_report.txt"
-# with open("summary_report.txt","w" ) as file:
-#     result=str(main())
-#     file.write("{result}")
-
-
-from pathlib import Path
-import csv
-def main():
-    from overheads import find_highest_overhead
-    from cash_on_hand import cash_on_hand
-    from profit_loss import profit_loss
-    return 
-main()
-
-
-# fp = Path.cwd()/"summary_report.txt"
-# with fp.open(mode="w", encoding="UTF-8", newline="") as file:
-#     file.write(main())
-        
-with open("summary_report.txt","w" ) as file:
-    file.write(f"{main()}")
-
