43c458cc6aed00992980251e0c3ae5b16cab4fc2
jonathan-galileo <139736148+jonathan-galileo@users.noreply.github.com>
2023-08-08 13:55:48 +0800

    Updated profit_loss to include comments

--- PFB_IGP.py
+++ PFB_IGP.py
@@ -1,4 +1,3 @@
-#profit and loss 
 from pathlib import Path
 import csv
 fp = Path.cwd()/'csv_reports'/"Profit_And_Loss.csv"
@@ -13,7 +12,7 @@ def profit_loss_function():
     '''
     This function is for calculating the difference in the net profit column and prints out the day and amount the highest increment occurs
     '''
-    difference_in_net_profit = []
+    difference_in_net_profit = [] # Creates an empty list to store
     # creates a for loop that iterates over a sequence of numbers starting from 1 up to the length of the "Profit_And_Loss" dataset
     for day in range(1, len(Profit_And_Loss)): 
          # retrieves the profit value for the current day and converts it to an integer, and assign it to the variable current_day_profit
@@ -24,47 +23,8 @@ def profit_loss_function():
         difference = prev_day_profit - current_day_profit
 
         if difference > 0:
-           difference_in_net_profit.append(f"[PROFIT DEFICIT] DAY: {Profit_And_Loss[day][0]}, AMOUNT: USD{difference}")
-    return difference_in_net_profit
-# Call the function
-profit_loss_function()
+           difference_in_net_profit.append(f"[PROFIT DEFICIT] DAY: {Profit_And_Loss[day][0]}, AMOUNT: USD{difference}") #Appends the deficit  
+    print(difference_in_net_profit) # Prints list to check if items in the list were printed out correctly 
+    return difference_in_net_profit # Returns the difference_in_net_profit list to the function
 
-
-
-
-
-
-
-
-
-MAIN
-from cash_on_hand import cash_on_hand_function
-from overheads import find_highest_overhead
-from profit_loss import profit_loss_function
-
-def main ():
-    Cash_On_Hand = cash_on_hand_function()
-    Overheads = find_highest_overhead()
-    Profit_Loss = profit_loss_function()
-    
-    print(Cash_On_Hand)
-    print(Overheads)
-    print(Profit_Loss)
-
-    cash_deficit_lines = ""
-    for deficit in Cash_On_Hand:
-        cash_deficit_lines += deficit + "\n"
-
-    profit_loss_lines = ""
-    for profit in Profit_Loss:
-         profit_loss_lines += profit + "\n"
-         
-
-    output = f"{cash_deficit_lines}"
-    output += f"[HIGHEST OVERHEAD] {Overheads[0][0].upper()}: {Overheads[0][1]}%\n"
-    output += f"{profit_loss_lines}"
-    with open('output.txt', 'w') as file: # Writes the result to a text file 
-            file.write(output)
-
-main()
-
+profit_loss_function() # Call the function to perform the calculations and display the results    
