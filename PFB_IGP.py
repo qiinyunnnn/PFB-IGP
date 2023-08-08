cc7457f3ccff65f82ddbd9703e54cedff4378e67
jonathan-galileo <139736148+jonathan-galileo@users.noreply.github.com>
2023-08-08 13:48:50 +0800

    Updated cash_on_hand to include comments

--- PFB_IGP.py
+++ PFB_IGP.py
@@ -13,17 +13,17 @@ def cash_on_hand_function():
     '''
     This function is for calculating the difference in Cash-on-hand and prints out the day and amount the highest increment occurs
     '''
-    cash_deficit = [] #Creates an empty list to store 
+    cash_deficit = [] # Creates an empty list to store cash deficit data 
     # creates a loop that iterates over a sequence of numbers starting from 1 up to the length of the "Cash_On_Hand" dataset
     for day in range(1, len(Cash_On_Hand)):
-        current_day_cash = int(Cash_On_Hand[day][1])
-        prev_day_cash = int(Cash_On_Hand[day-1][1])
-        difference = prev_day_cash - current_day_cash
+        current_day_cash = int(Cash_On_Hand[day][1]) # Finds the current day's amount 
+        prev_day_cash = int(Cash_On_Hand[day-1][1]) # Finds the previous day's amount
+        difference = prev_day_cash - current_day_cash # Calculates the difference 
 
-        if difference > 0:
+        if difference > 0: # If the difference is >0, meaning there is a cash deficit
             cash_deficit.append(f"[CASH DEFICIT] DAY: {Cash_On_Hand[day][0]}, AMOUNT: USD{difference}")
-    print(cash_deficit)
-    return cash_deficit
-# Call the function
-cash_on_hand_function()
+    print(cash_deficit) # Print list to check if the items in the list were printed out correctly 
+    return cash_deficit # Returns the cash_deficit list to the function
+
+cash_on_hand_function() # Call the function to perform the calculations and display the results 
 
