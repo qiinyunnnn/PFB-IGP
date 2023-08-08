ee7dc3e16efac5d41e7dea0c56bec6c3a58c54f0
jonathan-galileo <139736148+jonathan-galileo@users.noreply.github.com>
2023-08-08 14:09:08 +0800

    Updated main to include comments

--- PFB_IGP.py
+++ PFB_IGP.py
@@ -1,32 +1,23 @@
 
-MAIN
 from cash_on_hand import cash_on_hand_function
 from overheads import find_highest_overhead
 from profit_loss import profit_loss_function
 
 def main ():
-    Cash_On_Hand = cash_on_hand_function()
+    Cash_On_Hand = cash_on_hand_function() # Assigns each function to a variable 
     Overheads = find_highest_overhead()
     Profit_Loss = profit_loss_function()
-    
-    print(Cash_On_Hand)
-    print(Overheads)
-    print(Profit_Loss)
-
-    cash_deficit_lines = ""
-    for deficit in Cash_On_Hand:
-        cash_deficit_lines += deficit + "\n"
-
-    profit_loss_lines = ""
-    for profit in Profit_Loss:
-         profit_loss_lines += profit + "\n"
-         
-
-    output = f"{cash_deficit_lines}"
-    output += f"[HIGHEST OVERHEAD] {Overheads[0][0].upper()}: {Overheads[0][1]}%\n"
+    cash_deficit_lines = "" # Formats the cash_on_hand_function to write each Day and Amount pair to it's own line 
+    for cash_deficit in Cash_On_Hand:
+        cash_deficit_lines += cash_deficit + "\n"
+    profit_loss_lines = "" # Formats the profit_loss_function to write each Day and Amount pair to it's own line 
+    for profit_deficit in Profit_Loss:
+         profit_loss_lines += profit_deficit + "\n"
+    output = f"[HIGHEST OVERHEAD] {Overheads[0][0].upper()}: {Overheads[0][1]}%\n" # Creates output variable to store the different functions 
+    output += f"{cash_deficit_lines}"
     output += f"{profit_loss_lines}"
-    with open('output.txt', 'w') as file: # Writes the result to a text file 
-            file.write(output)
+    with open('output.txt', 'w') as file:
+            file.write(output)  # Writes the result to a text file 
 
-main()
+main() # Calls the function 
 
